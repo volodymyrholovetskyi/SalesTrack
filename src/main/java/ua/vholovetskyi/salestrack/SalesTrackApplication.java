cffbca858b0e6fd07e09efbd2b2d1cd3d0338527
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-11-17 18:10:30 +0100

    Init commit

---
+++ src/main/java/ua/vholovetskyi/salestrack/SalesTrackApplication.java
@@ -0,0 +1,13 @@
+package ua.vholovetskyi.salestrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/vholovetskyi/salestrack/SalesTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vholovetskyi.salestrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
